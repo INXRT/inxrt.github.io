52db83261506f69a070dd52912a9248c3a08e522
INXRT <inxrtmusic@gmail.com>
2026-02-08 16:48:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
 // Mobile Menu Toggle
-document.getElementById("mobile-menu").addEventListener("click", function() {
-  document.querySelector(".nav-links").classList.toggle("active");
+const menuToggle = document.getElementById("mobile-menu");
+const navLinks = document.querySelector(".nav-links");
+const links = document.querySelectorAll(".nav-links li a");
+
+// Toggle Menu
+if (menuToggle) {
+    menuToggle.addEventListener("click", function() {
+        navLinks.classList.toggle("active");
+    });
+}
+
+// Close menu when a link is clicked
+links.forEach(link => {
+  link.addEventListener("click", () => {
+    navLinks.classList.remove("active");
+  });
 });
+
